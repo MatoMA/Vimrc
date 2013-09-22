85ee37aee2fdbabf460bcc431842cb2613482a58
MatoMA <mato.q0o0p@gmail.com>
2013-09-22 14:55:53 +0200

    add auto_install

---
+++ auto_install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+if [ -f ~/.vimrc ]
+then 
+    echo "Backup .vimrc --> .vimrc_backup"
+    mv ~/.vimrc ~/.vimrc_backup
+fi
+
+echo "Update .vimrc"
+cp ./vimrc ~/.vimrc
+
+if [ -d ~/.vim/after ]
+then 
+    echo "Backup .vim/after --> .vim/after_backup"
+    rm -r ~/.vim/after_backup
+    mv ~/.vim/after ~/.vim/after_backup
+fi
+
+echo "Update .vim/after"
+cp -r ./after ~/.vim/after
+
+if [ ! -d ~/.vim/bundle/vundle ]
+then
+    echo "Install vundle"
+    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+else
+    vim +BundleClean +qall
+fi
+
+echo "Install plugins"
+vim +BundleInstall +qall
